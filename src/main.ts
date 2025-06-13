a2aa52915f3c38fe1ad2ead387e1c14385a49102
weizhixiang <994655681@qq.com>
2025-06-13 17:44:39 +0800

    vuex 更改（createStore）

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,6 @@ const app = createApp(App);
 // })
 
 app.use(router);
-// app.use(store);
+app.use(store);
 
 app.mount("#app");
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,31 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
+import { createStore } from "vuex";
 
 const state: IState = {
   loadingShow: false,
-  searchKeyWrold: window.sessionStorage.getItem('searchKeyWrold')
-    ? window.sessionStorage.getItem('searchKeyWrold')
-    : '',
+  searchKeyWrold: window.sessionStorage.getItem("searchKeyWrold")
+    ? window.sessionStorage.getItem("searchKeyWrold")
+    : "",
   searchHistory: JSON.parse(
-    window.localStorage.getItem('musicHistorySearch') || '[]'
+    window.localStorage.getItem("musicHistorySearch") || "[]"
   ),
-  loginAccount: window.sessionStorage.getItem('loginAccount') ? window.sessionStorage.getItem('loginAccount') : '',
-  account: JSON.parse((<any>window.localStorage).getItem('account')) || {},
-  playList: JSON.parse(window.localStorage.getItem('playlist') || '[]'),  // 播放列表中的容器
-  currentPlayIndex: (<any>window.localStorage).getItem('playIndex') ? parseInt((<any>window.localStorage).getItem('playIndex')) : -1,
+  loginAccount: window.sessionStorage.getItem("loginAccount")
+    ? window.sessionStorage.getItem("loginAccount")
+    : "",
+  account: JSON.parse((<any>window.localStorage).getItem("account")) || {},
+  playList: JSON.parse(window.localStorage.getItem("playlist") || "[]"), // 播放列表中的容器
+  currentPlayIndex: (<any>window.localStorage).getItem("playIndex")
+    ? parseInt((<any>window.localStorage).getItem("playIndex"))
+    : -1,
   // currentPlayIndex: parseInt((<any>window.localStorage).getItem('playIndex')) || -1, // 当前播放容器的索引值
-  playMode: parseInt((<any>window.localStorage).getItem('mode')) || EPlayMode.listLoop,
-  playHistory: JSON.parse(window.localStorage.getItem('playHistory') || '[]')
-}
-import { IState, EPlayMode } from './interface'
-import { mutations } from './mutatioins'
-import { actions } from './actions'
-import getters from './getters'
-export default new Vuex.Store({
+  playMode:
+    parseInt((<any>window.localStorage).getItem("mode")) || EPlayMode.listLoop,
+  playHistory: JSON.parse(window.localStorage.getItem("playHistory") || "[]"),
+};
+import { IState, EPlayMode } from "./interface";
+import { mutations } from "./mutatioins";
+import { actions } from "./actions";
+import getters from "./getters";
+export default createStore({
   state,
   mutations,
   actions,
-  getters
-})
+  getters,
+});
